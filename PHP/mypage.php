5b50e886a05f7c8450347b66b4497bc51b6b56a6
asdfqwer0910 <115201468+asdfqwer0910@users.noreply.github.com>
2022-10-24 11:01:13 +0900

    Update mypage.php
    マイページ作成開始

---
+++ PHP/mypage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mypage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div name="maindiv" class="container">
+        <div class="row">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-left">マイページ</p><br>
+                </div> 
+        </div>
+            </div>
+                    <p class="text-left">登録情報</p><br>
+                    <p class="text-left">閲覧履歴</p></br>
+                    <p class="text-left">予約照会</p><br>
+                    <p class="text-left">ログアウト</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
