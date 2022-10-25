163d55247c18e640939734bc55b45b7894756166
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-10-25 10:39:54 +0900

    決済方法選択画面　作成開始

--- WEB/paymentMethod.php
+++ WEB/paymentMethod.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 </head>
-<body>
-    <div class="container-fluid" style="background-color: #DFDFDFDF;">
+<body style="background-color: #DFDFDFDF;">
+    <div class="container-fluid">
         <dvi class="bg-white">決済方法選択画面</dvi>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
