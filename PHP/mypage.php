83f39ff2eb68b3cdbcdec333651193ff054465d9
asdfqwer0910 <115201468+asdfqwer0910@users.noreply.github.com>
2022-10-26 10:59:26 +0900

    マイページ作成！！！

--- PHP/mypage.php
+++ PHP/mypage.php
@@ -5,18 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mypage</title>
+    <link rel="stylesheet" href="C:/xampp/htdocs/TICKEPATH/CSS/mypage.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #DFDFDFDF;">
     <div name="maindiv" class="container">
         <div class="row">
+            <div class="p-3">
             <ul class="list-group">
                 <li class="list-group-item">マイページ</li><br>
-                <li class="list-group-item">登録情報</li>
-                <li class="list-group-item">閲覧履歴</li>
-                <li class="list-group-item">予約照会</li>
-                <li class="list-group-item">ログアウト</li>
+                <li class="list-group-item border border-dark">
+                <a href="home.php">登録情報</li></a>
+                <li class="list-group-item border border-dark">
+                <a href="home.php">閲覧履歴</li></a>
+                <li class="list-group-item border border-dark">予約照会</li>
+                <li class="list-group-item border border-dark">ログアウト</li><br>
             </ul>
+            <div class="d-grid gap-2 col-10 mx-auto">
+            <button type="button" class="btn btn-info btn-sm text-white border-dark"><a href="home.php">ホームへ戻る</button></a>
+            </div>
+        </div>
         </div>
     </div>
     <?php
