2688ea4c8c8dcf4fe70dd185778aba701d7522e3
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-10-28 14:29:32 +0900

    インサート文を追加（performance）

---
+++ SQL/insert_performance.sql
@@ -0,0 +1,20 @@
+/*INSERT INTO syain(id,name,romaji) VALUES (1,'鈴木','suzuki');*/
+INSERT INTO performance(
+    performance_name, 
+    artist_name,
+    place,
+    performance_date,
+    open_time,
+    start_time,
+    image_path
+)
+VALUES(
+    'AIMYON弾き語りTOUR 2021 “傷と悪魔と恋をした！',
+    'あいみょん',
+    '日本武道館',
+    '2021-11-30',
+    '18:30',
+    '19:00',
+    'https://localhost/TICKEPATH/IMAGES/PERFORMANCE/aimyon_live_image.png'
+);
+
