a76d13b098169f5a8fa8bf23b5387c79ff99dd09
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-10-28 13:52:35 +0900

    SQLにオートインクリメントを追加

---
+++ SQL/drop_table.sql
@@ -0,0 +1,9 @@
+DROP TABLE booking_detail;
+DROP TABLE seat;
+DROP TABLE history;
+DROP TABLE favorite;
+DROP TABLE booking;
+DROP TABLE client;
+DROP TABLE performance;
+DROP TABLE artist;
+DROP TABLE seat_value;
