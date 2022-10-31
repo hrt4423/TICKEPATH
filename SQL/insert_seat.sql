7c0be154b26cad2b58754de86e10ce0dde0cd6ae
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-10-31 10:34:53 +0900

    インサート文作成（席）

--- SQL/insert_seat.sql
+++ SQL/insert_seat.sql
@@ -1,5 +1,9 @@
-INSERT INTO seat(
-
-)VALUES(
-    
-)
+INSERT INTO seat
+(performance_id,seat_value_id,seat_price,is_vacant)
+VALUES
+(1, 2, 8800, 1),
+(1, 3, 8000, 1),
+(1,4,7500,1),
+(2, 2, 8800, 1),
+(2, 3, 8000, 1),
+(2, 4, 7500, 1);
