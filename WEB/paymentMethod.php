daced44dfad9e1176bc02ac2d65cdeb374eb048e
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-11-02 10:57:17 +0900

    微修正

--- WEB/paymentMethod.php
+++ WEB/paymentMethod.php
@@ -8,8 +8,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 </head>
-<body style="background-color: #DFDFDFDF;">
-    <div class="container-fluid">
         <div class="row">
             <h3 class="bg-white col-md-12 text-center">決済方法の選択</h3>
         
