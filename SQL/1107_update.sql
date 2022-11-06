b3edf01d7b0e38396c0343b2563c584d29cb95a4
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-11-06 18:20:35 +0900

    performanceクラスにコメントを追加

--- SQL/1107_update.sql
+++ SQL/1107_update.sql
@@ -4,6 +4,7 @@
     ・bookingテーブルに'is_cancel'列を追加（予約のキャンセル状況を表す, boolean型）
     ・seatテーブルの列'is_vacant'(INT型)を'is_reserved'(boolean型)に変更
     ・historyテーブルにbrowse_dateを追加（閲覧日時, DATETIME型）
+    ・booking, booking_detailテーブルにデータを追加
 */
 START TRANSACTION;
 
@@ -17,5 +18,28 @@ ALTER TABLE seat CHANGE is_vacant is_reserved BOOLEAN DEFAULT false;
 ALTER TABLE booking MODIFY is_cancel BOOLEAN DEFAULT false;
 
 ALTER TABLE history ADD COLUMN browse_date DATETIME;
+
+INSERT INTO booking(booking_date, client_id)
+VALUES('2021-10-30', 1),
+('2021-10-30', 2),
+('2021-10-30', 3),
+
+('2019-02-10', 1),
+('2019-02-10', 2),
+('2019-02-10', 3);
+
+INSERT INTO booking_detail(
+    booking_id,
+    visitor_family_name,
+    visitor_first_name,
+    seat_id
+)VALUES
+(1, '平山', '武', 1),
+(2, '前野', '武美', 2),
+(3, '志度', '朝日', 3),
+
+(4, '平山', '武', 4),
+(5,'前野', '武美', 5),
+(6, '志度', '朝日', 6);
 
 COMMIT;
--- WEB/home.php
+++ WEB/home.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../CSS/home.css">
     <title>ホーム画面</title>
 </head>
+
 <body style="background-color: #DFDFDF;">
     <?php
         require_once './DAO/performance.php';
@@ -24,41 +26,58 @@
             </div>
         </div>
 
-            <div id="carousel_bg">
-                <div id="carousel">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
+        <div id="alert">
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                <i class="bi bi-bell-fill position-relative">
+                    
+                </i>
+
+                <span id="alert_message">
+                    <strong>公演が近づいています！
+                        <a href="https://localhost/TICKEPATH/WEB/bookingInfo.php" class="text-decoration-none">
+                            確認する
+                        </a>
+                    </strong>
+                </span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div><!--alert-->
+
+        <div id="carousel_bg"><!--画像スライド-->
+            <div id="carousel">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+            
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../IMAGES/PERFORMANCE/aimyon_live_image.png" class="d-block mx-auto" alt="...">
                             </div>
-                
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="../IMAGES/PERFORMANCE/aimyon_live_image.png" class="d-block mx-auto" alt="...">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="../IMAGES/PERFORMANCE/yonedu_live_image.jpg" class="d-block mx-auto" alt="...">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="../IMAGES/PERFORMANCE/vaundy.jpg" class="d-block mx-auto" alt="...">
-                                </div>
+                            <div class="carousel-item">
+                                <img src="../IMAGES/PERFORMANCE/yonedu_live_image.jpg" class="d-block mx-auto" alt="...">
                             </div>
-                
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                    </div>
+                            <div class="carousel-item">
+                                <img src="../IMAGES/PERFORMANCE/vaundy.jpg" class="d-block mx-auto" alt="...">
+                            </div>
+                        </div>
+            
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                 </div>
             </div>
+        </div><!--画像スライド-->
         
         
-
         <div class="row">
             <div class="col-12" id="headline">
                 <h4 class="">
@@ -148,6 +167,58 @@
                 </div><!-- card-body -->
             </div><!-- card -->
         </div><!-- カード位置調整 -->
+
+        <div class="row">
+            <div class="col-12" id="headline">
+                <h4 class="">
+                    お気に入りアーティストの公演
+                </h4>
+            </div>
+        </div><!--row-->
+
+        <div class="card_position"><!--カード位置調整-->
+            <div class="card">
+                <div class="card-body">
+                    <div class="row gx-0">
+                        <div class="col-3" >
+                            <?php 
+                                $daoPerformance->outPutDate($aimyon);
+                            ?>
+                        </div>
+
+                        <div class="col-1">
+                            <div id="vertical_line">
+                                <!-- 縦線-->
+                            </div>
+                        </div>
+
+                        <div class="col-8">
+                            <h6 class="card-title">
+                                <?php
+                                    $daoPerformance->outPutArtist($aimyon);
+                                ?>
+                            </h6>
+                            <div>
+                                <?php
+                                    $daoPerformance->outPutPlace($aimyon);
+                                ?>
+                            </div>
+                            <div>
+                                <?php
+                                    echo '開演：';
+                                    $daoPerformance->outPutStartTime($aimyon);
+                                    echo '～';
+                                    echo '（開場', $daoPerformance->outPutOpenTime($aimyon), '～）';
+                                ?>
+                            </div>
+                        </div>
+                    </div><!--row-->
+                </div><!-- card-body -->
+            </div><!-- card -->
+        </div><!-- カード位置調整 -->
+
+
+
 
         <div class="row">
             <div class="col-12" id="footer">
